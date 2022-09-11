0d71946af3f0f9626d005ab4833108df27430360
Praneeth <praneethkrishna27@hotmail.com>
2022-09-12 01:24:20 +0530

    added dev1 modifications

--- app2.py
+++ app2.py
@@ -3,7 +3,7 @@ now, I have added some changes locally
 parallelly in the remote other developers have pushed the code
 
 def new_code():
-  print('added a new functionality')
+  print('added a new functionality by dev1')
 
 def added_another_module():
     print('dev2 has added another module')
