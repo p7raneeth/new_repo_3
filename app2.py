3c585c238d137bd9a53cd22dd1c44f57ba949f73
Praneeth <30497716+p7raneeth@users.noreply.github.com>
2022-09-12 01:22:19 +0530

    added a new function

--- app2.py
+++ app2.py
@@ -1,3 +1,6 @@
 some code related to the project, please clone this
 now, I have added some changes locally
 parallelly in the remote other developers have pushed the code
+
+def new_code():
+  print('added a new functionality')
